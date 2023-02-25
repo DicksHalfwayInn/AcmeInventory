c6fff26893e3191d5b5149b6a678ac8d946b1123
Schofield, stuart <546wine@gmail.com>
2023-02-25 16:22:35 -0700

    Added the Dependency property to the view to call the method when the SelectedTab value changes in the viewmodel It isn't working yet

--- AcmeInventory/AcmeInventory/ViewModels/MainViewModel.cs
+++ AcmeInventory/AcmeInventory/ViewModels/MainViewModel.cs
@@ -1,6 +1,12 @@
-﻿using AcmeInventory.DataAccess;
-using AcmeInventory.Models;
+﻿//using AcmeInventory.DataAccess;
+//using AcmeInventory.Models;
 using Avalonia;
+//using CommunityToolkit.Mvvm.ComponentModel;
+//using CommunityToolkit.Mvvm.Input;
+//using System;
+//using System.Collections.ObjectModel;
+
+using AcmeInventory.Models;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using System;
@@ -13,23 +19,15 @@ namespace AcmeInventory.ViewModels
         /// <summary>
         /// Enum to indicate which tab is currently selected
         /// </summary>
-        public enum ActiveTab
+        public enum TabType
         {
             Customer,
             Invoice,
             Inventory
         }
 
-        // Opacity of a tab that is unselected
-        private static double unselectedButtonOpacity = .65;
-
-        // Opacity of a tab that is selected
-        private static double selectedButtonOpacity = 1;
-
-        private static CornerRadius unselectedButtonCornerRadius =
-            new CornerRadius(5, 5, 5, 5);
-
-        private static CornerRadius selectedButtonCornerRadius = new CornerRadius(0, 0, 0, 0);
+        [ObservableProperty]
+        private TabType selectedTab;
 
         /// <summary>
         /// The Customers to display
@@ -55,24 +53,6 @@ namespace AcmeInventory.ViewModels
         [ObservableProperty]
         private ObservableCollection<InvoiceDetail> selectedInvoiceDetails;
 
-        /// <summary>
-        /// The opacity of the Invoice tab
-        /// </summary>
-        [ObservableProperty]
-        private double invoicesButtonOpacity = unselectedButtonOpacity;
-
-        /// <summary>
-        /// The opacity of the Customer tab
-        /// </summary>
-        [ObservableProperty]
-        private double customersButtonOpacity = unselectedButtonOpacity;
-
-        /// <summary>
-        /// The opacity of the Inventory tab
-        /// </summary>
-        [ObservableProperty]
-        private double inventoryButtonOpacity = unselectedButtonOpacity;
-
         [ObservableProperty]
         private bool customersTabIsOpen = false;
 
@@ -81,15 +61,6 @@ namespace AcmeInventory.ViewModels
 
         [ObservableProperty]
         private bool invoicesTabIsOpen = false;
-
-        [ObservableProperty]
-        private CornerRadius customersTabCornerRadius = unselectedButtonCornerRadius;
-
-        [ObservableProperty]
-        private CornerRadius invoicesTabCornerRadius = unselectedButtonCornerRadius;
-
-        [ObservableProperty]
-        private CornerRadius inventoryTabCornerRadius = unselectedButtonCornerRadius;
 
         /// <summary>
         /// Default Constructor
@@ -107,57 +78,40 @@ namespace AcmeInventory.ViewModels
         public void TabButtonPressed(string buttonPressed)
         {
             // Convert the string parameter to an ActiveTab enum
-            Enum.TryParse(buttonPressed, out ActiveTab tabButton);
+            Enum.TryParse(buttonPressed, out TabType selectedTab);
+
+            SelectedTab = selectedTab;
 
             // Switch on the parameter enum
-            switch (tabButton)
+            switch (selectedTab)
             {
                 // Customer tab was pressed
-                case ActiveTab.Customer:
+                case TabType.Customer:
                     {
-                        // Highlight the Customer tab and dim the others
-                        CustomersButtonOpacity = selectedButtonOpacity;
-                        InvoicesButtonOpacity = unselectedButtonOpacity;
-                        InventoryButtonOpacity = unselectedButtonOpacity;
                         CustomersTabIsOpen = true;
                         InvoicesTabIsOpen = false;
                         InventoryTabIsOpen = false;
-                        CustomersTabCornerRadius = selectedButtonCornerRadius;
-                        InvoicesTabCornerRadius = unselectedButtonCornerRadius;
-                        InventoryTabCornerRadius = unselectedButtonCornerRadius;
 
                         break;
                     }
 
                 // Invoice tab was pressed
-                case ActiveTab.Invoice:
+                case TabType.Invoice:
                     {
-                        // Hightlight the Invoice tab and dim the others
-                        InvoicesButtonOpacity = selectedButtonOpacity;
-                        CustomersButtonOpacity = unselectedButtonOpacity;
-                        InventoryButtonOpacity = unselectedButtonOpacity;
                         InvoicesTabIsOpen = true;
                         CustomersTabIsOpen = false;
                         InventoryTabIsOpen = false;
-                        InvoicesTabCornerRadius = selectedButtonCornerRadius;
-                        CustomersTabCornerRadius = unselectedButtonCornerRadius;
-                        InventoryTabCornerRadius = unselectedButtonCornerRadius;
+
                         break;
                     }
 
                 // Inventory tab was pressed
-                case ActiveTab.Inventory:
+                case TabType.Inventory:
                     {
-                        // Hightlight the Inventory tab and dim the others
-                        InventoryButtonOpacity = selectedButtonOpacity;
-                        CustomersButtonOpacity = unselectedButtonOpacity;
-                        InvoicesButtonOpacity = unselectedButtonOpacity;
                         InventoryTabIsOpen = true;
                         CustomersTabIsOpen = false;
                         InvoicesTabIsOpen = false;
-                        InventoryTabCornerRadius = selectedButtonCornerRadius;
-                        CustomersTabCornerRadius = unselectedButtonCornerRadius;
-                        InvoicesTabCornerRadius = unselectedButtonCornerRadius;
+
                         break;
                     }
             }
--- AcmeInventory/AcmeInventory/Views/MainView.axaml.cs
+++ AcmeInventory/AcmeInventory/Views/MainView.axaml.cs
@@ -1,12 +1,43 @@
+using Avalonia;
 using Avalonia.Controls;
+using static AcmeInventory.ViewModels.MainViewModel;
 
 namespace AcmeInventory.Views
 {
     public partial class MainView : UserControl
     {
+        // Opacity of a tab that is unselected
+        private static double unselectedButtonOpacity = .65;
+
+        // Opacity of a tab that is selected
+        private static double selectedButtonOpacity = 1;
+
+        // Corner radius of a tab that is unselected
+        private static CornerRadius unselectedButtonCornerRadius =
+            new CornerRadius(5, 5, 5, 5);
+
+        // Corner radius of a tab that is selected
+        private static CornerRadius selectedButtonCornerRadius = new CornerRadius(0, 0, 0, 0);
+
         public MainView()
         {
             InitializeComponent();
+        }
+
+        public static readonly StyledProperty<TabType> SelectedTabProperty =
+            AvaloniaProperty.Register<MainView, TabType>(nameof(ActiveTabDP));
+
+        public TabType ActiveTabDP
+        {
+            get
+            {
+                return GetValue(SelectedTabProperty);
+            }
+
+            set
+            {
+                SetValue(SelectedTabProperty, value);
+            }
         }
     }
 }
